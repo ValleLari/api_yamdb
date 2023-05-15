176fe6ab6366a64223998a3892a5d3feda8448e6
ValleLari <120548629+ValleLari@users.noreply.github.com>
2023-05-15 23:36:55 +0300

    Revert "First commit reviews_comments"

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -13,6 +13,7 @@ ALLOWED_HOSTS = ['*']
 
 
 # Application definition
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,31 +1,3 @@
 from django.contrib import admin
-from .models import Review, Comment
 
-
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = (
-        'author',
-        'title',
-        'text',
-        'score',
-        'pub_date'
-    )
-    search_fields = ('title',)
-    list_filter = ('author', 'title')
-    empty_value_display = '-пусто-'
-
-
-class CommentAdmin(admin.ModelAdmin):
-    list_display = (
-        'author',
-        'review',
-        'text',
-        'pub_date'
-    )
-    search_fields = ('review',)
-    list_filter = ('author', 'review')
-    empty_value_display = '-пусто-'
-
-
-admin.site.register(Review, ReviewAdmin)
-admin.site.register(Comment, CommentAdmin)
+# Register your models here.
